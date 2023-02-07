6bfac64d3d723a9d4133019741b311efec895f63
bobombolo <bobombolo@users.noreply.github.com>
2023-02-07 14:44:33 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -1202,7 +1202,7 @@ function automata.show_rc_form(pname)
 				patterns = 	patterns..","..minetest.formspec_escape(k
 							.." ["..v.status.."] gen:"..v.iteration.." cells:"
 							..v.cell_count.." time:"..math.ceil(v.t_timer).."ms min:"
-							..(pmin.x or "nil").."."..(pmin.yor "nil").."."..(pmin.z or "nil")
+							..(pmin.x or "nil").."."..(pmin.y or "nil").."."..(pmin.z or "nil")
 							.." max:"..(pmax.x or "nil").."."..(pmax.y or nil).."."..(pmax.z or "nil") )
 				automata.open_tab5[pname][i]=k --need this table to decode the form's pid_ids back to pattern_ids
 			end
